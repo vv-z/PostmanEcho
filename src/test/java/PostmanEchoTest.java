d1b9b15670da801290bf1c79fb6d36995ba5f897
Vitaliy Z <vv-z@mail.ru>
2023-06-25 13:23:03 +0300

    Initinal commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,37 @@
+import static io.restassured.RestAssured.basePath;
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.restassured.internal.RequestSpecificationImpl;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matcher;
+import org.junit.jupiter.api.Test;
+
+import java.net.http.HttpResponse;
+
+import static org.hamcrest.Matchers.equalTo;
+import static io.restassured.RestAssured.given;
+
+class PostmanEchoTest {
+    private HttpResponse<Object> ValidatableResponse;
+    private Object expectedStatusCode;
+
+    @Test
+    void shouldReturnSendData() {
+        given()
+
+                .baseUri("https://postman-echo.com")
+                .body("25.06.2023")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("25.06.2023"));
+
+
+    }
+
+    private void body(String path, Matcher<String> hi) {
+    }
+
+
+}
